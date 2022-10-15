eb1b12a7cf1d5c033fb37fc85d6e633324f2de3e
Kamil <prospero.apps@gmail.com>
2022-10-15 22:27:50 +0200

    Implement basic functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    title: 'Welcome message',
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date()
+  },
+  {
+    title: 'Hellow World',
+    text: "Yes, just hello world, that's it.",
+    user: "Charles",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages });
+});
+
+/* GET new message page. */
+router.get('/new', function(req, res, next) {
+  res.render('form', {title: 'Create New Message'});
+});
+
+/* POST new message page. */
+router.post('/new', function(req, res, next) {
+  messages.push({
+    title: req.body.messageTitle,
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date()
+  })
+
+  res.redirect('/')
 });
 
 module.exports = router;
